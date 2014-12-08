396e32128aa07ce32b7a97b2ca28ebc1693bceb1
parkjaesung <javajigi@gmail.com>
2014-12-08 18:41:49 +0900

    자바 Generic을 활용해 캐스팅을 하지 않도록 라이브러리 구현 가변 인자를 활용해 개발자 편의성 개선 여러 건의 데이터를 조회할 수 있는 기능 추가

--- test/net/slipp/user/UserDAOTest.java
+++ test/net/slipp/user/UserDAOTest.java
@@ -2,11 +2,17 @@ package net.slipp.user;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class UserDAOTest {
+	private static final Logger logger = LoggerFactory.getLogger(UserDAOTest.class);
 
 	private UserDAO userDao;
 	
@@ -35,6 +41,13 @@ public class UserDAOTest {
 		userDao.removeUser(user.getUserId());
 		User dbUser = userDao.findByUserId(user.getUserId());
 		assertNull(dbUser);
+	}
+	
+	@Test
+	public void findUsers() throws Exception {
+		List<User> users = userDao.findUsers();
+		assertTrue(users.size() > 0);
+		logger.debug("Users : {}", users);
 	}
 
 }
